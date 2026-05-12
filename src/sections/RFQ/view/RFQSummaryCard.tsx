a2385a3c286128815c0c8c1205ea2f0d8b5357d8
Ajith Pradeep <ajith.p@gnxsolutions.in>
2026-05-12 19:57:08 +0530

    commit logo

--- src/sections/RFQ/view/RFQSummaryCard.tsx
+++ src/sections/RFQ/view/RFQSummaryCard.tsx
@@ -1,7 +1,8 @@
 'use client';
 
+import { Box, Paper, Button, Divider, Typography } from '@mui/material';
+
 import React from 'react';
-import { Box, Paper, Divider, Typography, Button } from '@mui/material';
 
 function RFQHeaderCard() {
   return (
